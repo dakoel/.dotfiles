9c1e1e090a5f68eadbb3f1520bf7c465fe0023fb
Daniel Ellert <dkedevelop@pm.me>
2022-06-20 13:49:38 +0200

    Added treesitter

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -13,10 +13,15 @@ require("packer").startup(function(use)
   }
   use {
     "kyazdani42/nvim-tree.lua",
-    requires = {"kyazdani42/nvim-web-devicons"}
+    requires = {"kyazdani42/nvim-web-devicons"},
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ':TSUpdate'
+  }
 }
 end)
 
 require("plugins/lsp")
 require("plugins/cmp")
 require("nvim-tree").setup()
+require("plugins/treesitter")
---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,9 @@
+require("nvim-treesitter.configs").setup({
+  ensure_installed = {
+    "dart",
+    "lua"
+  },
+  highlight = {
+    enable = true,
+  }
+})
