46239683e3cbb4b945f1259829587dc820ae4905
Daniel Ellert <dkedevelop@pm.me>
2022-06-20 11:19:00 +0200

    Keymaps for terminal

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,6 +1,11 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<Esc>", ":silent! nohls<CR>")
 
+-- Terminal
+vim.keymap.set("n", "<Leader>tt", "<Cmd>terminal<CR>")
+vim.keymap.set("n", "<Leader>th", "<Cmd>split | terminal<CR>")
+vim.keymap.set("n", "<Leader>tv", "<Cmd>vsplit | terminal<CR>")
+
 -- Telescope
 vim.keymap.set("n", "<Leader>ff", "<Cmd>Telescope find_files<CR>")
 vim.keymap.set("n", "<Leader>fg", "<Cmd>Telescope live_grep<CR>")
