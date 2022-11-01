1f95d735c99ee53df91b5192e16d611f390dff26
Daniel Ellert <dkedevelop@pm.me>
2022-11-01 15:25:00 +0100

    Indent folding for neovim

--- neovim/nvim/lua/options.lua
+++ neovim/nvim/lua/options.lua
@@ -17,5 +17,10 @@ vim.o.expandtab = true
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 
+-- Folding
+vim.o.foldmethod = "indent"
+vim.o.nofoldenable = true
+vim.o.foldlevel = 99
+
 -- Terminal
 vim.cmd("autocmd TermOpen * setlocal nonumber norelativenumber")
