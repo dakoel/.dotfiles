18b82f02d35e5b97c53dade31203191e859edf8f
Daniel Ellert <dkedevelop@pm.me>
2023-12-03 23:17:04 +0100

    Move toggle colorscheme keybinding

--- neovim/nvim/lua/config/keymaps.lua
+++ neovim/nvim/lua/config/keymaps.lua
@@ -2,11 +2,6 @@
 vim.keymap.set("n", "<CR>", ":")
 vim.keymap.set("i", "jk", "<ESC>")
 
--- Colorscheme
-vim.keymap.set("n", "_", function()
-  vim.cmd(vim.g.colors_name == "onedark" and "colorscheme onelight" or "colorscheme onedark")
-end, { desc = "Toggle colorscheme" })
-
 -- Terminal
 vim.keymap.set("n", "<Leader>tt", "<Cmd>terminal<CR>")
 vim.keymap.set("n", "<Leader>ts", "<Cmd>split | terminal<CR>")
--- neovim/nvim/lua/plugins/onedarkpro.lua
+++ neovim/nvim/lua/plugins/onedarkpro.lua
@@ -1,6 +1,16 @@
 return {
   {
     "olimorris/onedarkpro.nvim",
+    keys = {
+      {
+        "_",
+        function()
+          vim.cmd(vim.g.colors_name == "onedark" and "colorscheme onelight" or "colorscheme onedark")
+        end,
+        mode = "n",
+        desc = "Toggle colorscheme",
+      },
+    },
   },
   {
     "LazyVim/LazyVim",
