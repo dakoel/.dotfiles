39d6349dd025b173bee6e211aa3fc9a7f4d06a97
Daniel Ellert <daniel.ellert@secunet.com>
2023-12-07 09:38:20 +0100

    Enable debugging functionality for flutter

--- neovim/nvim/lua/plugins/flutter-tools.lua
+++ neovim/nvim/lua/plugins/flutter-tools.lua
@@ -5,5 +5,12 @@ return {
     "nvim-lua/plenary.nvim",
     "stevearc/dressing.nvim", -- optional for vim.ui.select
   },
-  config = true,
+  config = function()
+    require("flutter-tools").setup({
+      debugger = {
+        enabled = true,
+        run_via_dap = true,
+      },
+    })
+  end,
 }
