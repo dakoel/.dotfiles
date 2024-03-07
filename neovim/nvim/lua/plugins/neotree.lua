0446449c4b812610895e1048f6b5e2f9e76cf5f0
Daniel Ellert <daniel.ellert@secunet.com>
2024-03-07 15:34:30 +0100

    Style neotree

--- neovim/nvim/lua/plugins/neotree.lua
+++ neovim/nvim/lua/plugins/neotree.lua
@@ -1,8 +1,27 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    window = {
-      position = "float",
-    },
-  },
+  opts = function()
+    local icons = require("lazyvim.config").icons
+    return {
+      default_component_configs = {
+        git_status = {
+          symbols = {
+            added = icons.git.added,
+            modified = icons.git.modified,
+            deleted = icons.git.removed,
+            renamed = "󰁕",
+            -- Status type
+            untracked = "",
+            ignored = "",
+            unstaged = "󰄱",
+            staged = "",
+            conflict = "",
+          },
+        },
+      },
+      window = {
+        position = "float",
+      },
+    }
+  end,
 }
