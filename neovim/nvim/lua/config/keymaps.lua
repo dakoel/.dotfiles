76aed9aaaa43f27d1035de85f9ee54e53f715a27
Daniel Ellert <daniel.ellert@secunet.com>
2024-03-27 15:28:56 +0100

    Add Neovim keymaps for marks

--- neovim/nvim/lua/config/keymaps.lua
+++ neovim/nvim/lua/config/keymaps.lua
@@ -1,6 +1,19 @@
 -- General configuration
 vim.keymap.set("i", "jk", "<ESC>")
 
+-- Marks
+for i = string.byte("a"), string.byte("z") do
+  local char = string.char(i)
+  vim.keymap.set("n", "dm" .. char, "<Cmd>delmark " .. char .. "<CR>", { desc = "Delete mark " .. char })
+  vim.keymap.set(
+    "n",
+    "dm" .. string.upper(char),
+    "<Cmd>delmark " .. string.upper(char) .. "<CR>",
+    { desc = "Delete mark " .. string.upper(char) }
+  )
+end
+vim.keymap.set("n", "dm<Leader>", "<Cmd>delmark a-z A-Z<CR>", { desc = "Delete all marks" })
+
 -- Telescope
 vim.keymap.set("n", "<Leader><Space>", "<Cmd>Telescope find_files<CR>", { desc = "Find Files (cwd)" })
 
