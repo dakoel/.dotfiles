7ee147eeee1c7f0480ead9913c65ef8a906bab8f
Daniel Ellert <daniel@MacBook-Pro-von-Daniel.local>
2024-05-13 21:22:57 +0200

    Toggle theme without need of argument

--- toggle_theme.sh
+++ toggle_theme.sh
@@ -8,13 +8,11 @@ function write_theme_file() {
 	cp "$source_file" "$target_file"
 }
 
-if [ "$#" -ne 1 ]; then
-	echo "Usage: $0 {light|dark}"
-	exit 1
-fi
-
-write_theme_file "./alacritty/alacritty/alacritty_${1}.toml" "./alacritty/alacritty/alacritty.toml"
-write_theme_file "./lazygit/lazygit/config_${1}.yml" "./lazygit/lazygit/config.yml"
-write_theme_file "./neovim/nvim/lua/plugins/colorscheme_${1}" "./neovim/nvim/lua/plugins/colorscheme.lua"
-write_theme_file "./tmux/.tmux_${1}.conf" "./tmux/.tmux.conf"
+read -r current_theme <"./.theme"
+theme=$(if [[ "$current_theme" == "dark" ]]; then echo "light"; else echo "dark"; fi)
+echo "${theme}" >"./.theme"
+write_theme_file "./alacritty/alacritty/alacritty_${theme}.toml" "./alacritty/alacritty/alacritty.toml"
+write_theme_file "./lazygit/lazygit/config_${theme}.yml" "./lazygit/lazygit/config.yml"
+write_theme_file "./neovim/nvim/lua/plugins/colorscheme_${theme}" "./neovim/nvim/lua/plugins/colorscheme.lua"
+write_theme_file "./tmux/.tmux_${theme}.conf" "./tmux/.tmux.conf"
 tmux source-file "./tmux/.tmux.conf"
