0a160d627fcfd77b60fce9786d3aaac2213c09b5
Daniel Ellert <daniel@MacBook-Pro-von-Daniel.local>
2024-05-13 20:22:31 +0200

    Add toggle functionality for lazygit

--- toggle_theme.sh
+++ toggle_theme.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+function write_theme_file() {
+	local source_file="$1"
+	local target_file="$2"
+
+	rm -f "$target_file"
+	cp "$source_file" "$target_file"
+}
+
 if [ "$#" -ne 1 ]; then
 	echo "Usage: $0 {light|dark}"
 	exit 1
 fi
 
-SOURCE_THEME_FILE="./alacritty/alacritty/alacritty_${1}.toml"
-TARGET_THEME_FILE="./alacritty/alacritty/alacritty.toml"
-
-rm $TARGET_THEME_FILE
-cp $SOURCE_THEME_FILE $TARGET_THEME_FILE
+write_theme_file "./alacritty/alacritty/alacritty_${1}.toml" "./alacritty/alacritty/alacritty.toml"
+write_theme_file "./lazygit/lazygit/config_${1}.yml" "./lazygit/lazygit/config.yml"
