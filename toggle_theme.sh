5cc95d2eed22811e4a4f9ca51db2eff2cc37e9b7
Daniel Ellert <daniel@MacBook-Pro-von-Daniel.local>
2024-05-14 15:44:20 +0200

    Toggle macOS theme if possible

--- toggle_theme.sh
+++ toggle_theme.sh
@@ -13,6 +13,7 @@ function write_theme_file() {
 read -r current_theme <"${dir}/.theme"
 theme=$(if [[ "$current_theme" == "dark" ]]; then echo "light"; else echo "dark"; fi)
 gnome_theme=$(if [[ "$theme" == "light" ]]; then echo "default"; else echo "prefer-dark"; fi)
+macos_theme=$(if [[ "$theme" == "light" ]]; then echo "false"; else echo "true"; fi)
 echo "${theme}" >"${dir}/.theme"
 
 # Set new theme for CLI applications
@@ -25,4 +26,9 @@ tmux source-file "${dir}/tmux/.tmux.conf"
 # Set new theme for gnome
 if command -v gsettings &>/dev/null; then
 	gsettings set org.gnome.desktop.interface color-scheme ${gnome_theme}
+fi
+
+# Set new theme for macOS
+if command -v osascript &>/dev/null; then
+	osascript -e "tell application \"System Events\" to tell appearance preferences to set dark mode to ${macos_theme}"
 fi
