dedb35cd3e0e696e6d0f8cb3132db69b749fa3cf
Daniel Ellert <daniel.ellert@secunet.com>
2024-05-14 12:51:07 +0200

    Toggle gnome if installed

--- toggle_theme.sh
+++ toggle_theme.sh
@@ -9,11 +9,20 @@ function write_theme_file() {
 	cp "$source_file" "$target_file"
 }
 
+# Read current theme and prepare new theme
 read -r current_theme <"${dir}/.theme"
 theme=$(if [[ "$current_theme" == "dark" ]]; then echo "light"; else echo "dark"; fi)
+gnome_theme=$(if [[ "$theme" == "light" ]]; then echo "default"; else echo "prefer-dark"; fi)
 echo "${theme}" >"${dir}/.theme"
+
+# Set new theme for CLI applications
 write_theme_file "${dir}/alacritty/alacritty_${theme}.toml" "${dir}/alacritty/alacritty.toml"
 write_theme_file "${dir}/lazygit/config_${theme}.yml" "${dir}/lazygit/config.yml"
 write_theme_file "${dir}/nvim/lua/plugins/colorscheme_${theme}" "${dir}/nvim/lua/plugins/colorscheme.lua"
 write_theme_file "${dir}/tmux/tmux_${theme}.conf" "${dir}/tmux/.tmux.conf"
 tmux source-file "${dir}/tmux/.tmux.conf"
+
+# Set new theme for gnome
+if command -v gsettings &>/dev/null; then
+	gsettings set org.gnome.desktop.interface color-scheme ${gnome_theme}
+fi
