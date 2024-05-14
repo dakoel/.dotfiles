01024bcf3a06f7b82df528a7c85361ad4d4c9551
Daniel Ellert <daniel.ellert@secunet.com>
2024-05-14 09:14:57 +0200

    Additionally source local fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,7 @@
+set local_config ~/.config/fish/config.local.fish
+test -r $local_config; and source $local_config
+set -e local_config
+
 if status is-interactive
     if not command -q starship
         curl -sS https://starship.rs/install.sh | sh
