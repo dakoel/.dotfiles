ecc9f643c472e805126e2ce1161284195027da95
Daniel Ellert <daniel@MacBook-Pro-von-Daniel.local>
2024-05-15 17:39:21 +0200

    Add alias for note taking

--- fish/config.fish
+++ fish/config.fish
@@ -13,7 +13,9 @@ if status is-interactive
     alias la="ls -l -a"
     alias lg="lazygit"
     alias vim="nvim"
+
     alias tt="$HOME/.dotfiles/toggle_theme.sh"
+    alias n="mkdir -p $HOME/.notes && cd $HOME/.notes && nvim && cd -"
 
     starship init fish | source
 end
