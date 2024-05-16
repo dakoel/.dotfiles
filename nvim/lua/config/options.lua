04a306ea47eeea432bd8ba3a4b4bf6975748ff1c
Daniel Ellert <daniel.ellert@secunet.com>
2024-05-16 15:13:55 +0200

    Enable local nvim configs

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,4 +1,23 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local function load_project_specific_settings()
+  local nvim_lua = vim.fn.getcwd() .. "/.nvim.lua"
+  local file_handle = io.open(nvim_lua, "r")
+
+  if file_handle then
+    local contents = file_handle:read("*a")
+    file_handle:close()
+
+    local success, err = pcall(function()
+      return loadstring(contents)()
+    end)
+
+    if not success then
+      print("Error executing .nvim.lua:", err)
+    end
+  end
+end
+
 vim.opt.spell = false
+load_project_specific_settings()
