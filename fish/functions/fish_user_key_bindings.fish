3c185ad192f35cf50addde0b075fde7d243a43bd
Daniel Ellert <daniel.ellert@secunet.com>
2024-06-05 09:47:24 +0200

    Add fish keybinding to accept autosuggestion

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,6 @@
 fish_vi_key_bindings
 function fish_user_key_bindings
     bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
+    bind -M insert \ca "commandline -f accept-autosuggestion execute"
     bind -M default q exit
 end
