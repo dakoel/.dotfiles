f616ba06d868735a0560c9877f9e45b11d2ecfd8
Daniel Ellert <daniel.ellert@secunet.com>
2024-07-25 09:14:59 +0200

    Clean Neovim config

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -16,10 +16,3 @@ vim.keymap.set("n", "dm<Leader>", "<Cmd>delmark a-z A-Z<CR>", { desc = "Delete a
 
 -- Telescope
 vim.keymap.set("n", "<Leader><Space>", "<Cmd>Telescope find_files<CR>", { desc = "Find Files (cwd)" })
-
--- Terminal
-vim.keymap.set("n", "<Leader>tt", "<Cmd>terminal<CR>")
-vim.keymap.set("n", "<Leader>ts", "<Cmd>split | terminal<CR>")
-vim.keymap.set("n", "<Leader>tv", "<Cmd>vsplit | terminal<CR>")
-vim.cmd("autocmd TermOpen * tnoremap <ESC> <C-\\><C-n>")
-vim.cmd("autocmd TermOpen * tnoremap <Leader>jk <C-\\><C-n>")
